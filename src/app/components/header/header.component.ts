4a23a58ed5853eaec92457944cbc2a760ff8c671
Rafael Jardim <rafaelmjardim@gmail.com>
2024-01-24 22:54:17 -0300

    update

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -29,7 +29,7 @@ export class HeaderComponent implements OnInit{
         url: 'habilidades'
       },
       {
-        txt: 'projetos',
+        txt: 'Projetos',
         url: 'projetos'
       },
       {
