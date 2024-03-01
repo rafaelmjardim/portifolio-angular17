bae1da6aee5201d2180c677c39666089c3bf97eb
Rafael Jardim <rafaelmjardim@gmail.com>
2024-03-01 10:27:48 -0300

    update geral

--- src/app/pages/main/components/footer/footer.component.ts
+++ src/app/pages/main/components/footer/footer.component.ts
@@ -1,15 +1,49 @@
 import { NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SocialIcon } from './footer';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { ionLogoInstagram, ionLogoLinkedin, ionLogoWhatsapp, ionMailOutline, ionLogoGithub } from '@ng-icons/ionicons';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    NgIcon
   ],
+  providers: provideIcons({ionLogoInstagram, ionLogoLinkedin, ionLogoGithub, ionMailOutline, ionLogoWhatsapp}),
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  socialIconList: SocialIcon[] = [];
 
+  ngOnInit(): void {
+    this.initSocialIcons();
+  }
+
+  initSocialIcons = () => {
+    this.socialIconList = [
+      {
+        value: 'ionLogoInstagram',
+        color: '#F38C21'
+      },
+      {
+        value: 'ionLogoLinkedin',
+        color: '#F38C21'
+      },
+      {
+        value: 'ionLogoGithub',
+        color: '#F38C21'
+      },
+      {
+        value: 'ionMailOutline',
+        color: '#F38C21'
+      },
+      {
+        value: 'ionLogoWhatsapp',
+        color: '#F38C21'
+      },
+    ]
+  }
 }
