bc78c2073a86f1e329b97e9dc483f54695917bec
Rafael Jardim <rafaelmjardim@gmail.com>
2024-05-07 08:47:32 -0300

    feat: adicionado angular material

--- src/app/components/email-box/email-box.component.ts
+++ src/app/components/email-box/email-box.component.ts
@@ -2,11 +2,17 @@ import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { ionMailOutline } from "@ng-icons/ionicons";
+import { ClipboardModule } from '@angular/cdk/clipboard';
+
 
 @Component({
   selector: 'app-email-box',
   standalone: true,
-  imports: [NgIconComponent, NgClass],
+  imports: [
+    NgIconComponent, 
+    NgClass, 
+    ClipboardModule
+  ],
   providers: [provideIcons({ionMailOutline})],
   templateUrl: './email-box.component.html',
   styleUrl: './email-box.component.scss'
@@ -14,4 +20,15 @@ import { ionMailOutline } from "@ng-icons/ionicons";
 export class EmailBoxComponent {
 
   copyTxtEnable: boolean = false;
+
+  copyClicked: boolean = false;
+
+  emailTxt = 'rafaelmjardim@gmail.com'
+
+  handleCopy = () => {
+    this.copyClicked = !this.copyClicked;    
+
+    
+    
+  }
 }
