e7da9f3e0f3574a69716fc7bbe85713f7b2bb686
Rafael Jardim <rafaelmjardim@gmail.com>
2024-05-08 22:26:11 -0300

    adicionado animação na pagina home

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,18 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+//LottieFiles config
+import { provideLottieOptions } from 'ngx-lottie';
+import player from 'lottie-web';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes), 
+    provideClientHydration(), 
+    provideAnimationsAsync(),
+    
+    provideLottieOptions({
+      player: () => player,
+    }),
+  ],
 };
--- src/app/pages/main/components/home/home.component.ts
+++ src/app/pages/main/components/home/home.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../../../components/header/header.component';
 import { EmailBoxComponent } from '../../../../components/email-box/email-box.component';
+import { AnimationOptions, LottieComponent } from 'ngx-lottie';
+import { AnimationItem } from 'ngx-lottie/lib/symbols';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, EmailBoxComponent],
+  imports: [HeaderComponent, EmailBoxComponent, LottieComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  options: AnimationOptions = {
+    path: '/assets/lottie/animation.json',
+  }
 
+  animationCreated = (animationItem: AnimationItem): void => {
+    console.log(animationItem);
+    
+  }
 }
