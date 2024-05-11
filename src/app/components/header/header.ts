d734bcacb770595ab895de8c9f735bbfc654af57
Rafael Jardim <rafaelmjardim@gmail.com>
2024-05-11 01:04:15 -0300

    criado menu responsivo

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -7,3 +7,32 @@ export type Header = {
     pdf?: string
     isButton?: boolean
 }
+
+export const MENU_LIST: Header[] = [
+    {
+        txt: 'Início',
+        href: '#',
+    },
+    {
+        txt: 'Sobre',
+        href: '#about',
+    },
+    {
+        txt: 'Habilidades',
+        href: '#skills',
+    },
+    {
+        txt: 'Projetos',
+        href: '#projects',
+    },
+    {
+        txt: 'Contato',
+        href: '#footer',
+    },
+    {
+        txt: 'Currículo',
+        href: 'assets/curriculo_rafael_jardim.pdf',
+        icon: 'ionArrowDownOutline',
+        isButton: true
+    }
+]
--- src/app/components/menu-mobile/menu-mobile.component.ts
+++ src/app/components/menu-mobile/menu-mobile.component.ts
@@ -1,15 +1,25 @@
+import { CommonModule, NgClass, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { ionMenuOutline } from '@ng-icons/ionicons';
+import { ionCloseCircleOutline, ionCloseOutline, ionMenuOutline } from '@ng-icons/ionicons';
+import { Header, MENU_LIST } from '../header/header';
 
 @Component({
   selector: 'app-menu-mobile',
   standalone: true,
-  imports: [NgIcon],
-  providers: provideIcons({ionMenuOutline}),
+  imports: [NgIcon, NgClass],
+  providers: provideIcons({ionMenuOutline, ionCloseOutline}),
   templateUrl: './menu-mobile.component.html',
   styleUrl: './menu-mobile.component.scss'
 })
 export class MenuMobileComponent {
+
+  showMenu: boolean = false;
+
+  menuList: Header[] = MENU_LIST;
+
+  handleHideMenu = () => {    
+    this.showMenu = !this.showMenu;
+  }
 
 }
