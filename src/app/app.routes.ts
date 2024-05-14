1f372a7e71108662678eb2bd263bace95de7758e
Rafael Jardim <rafaelmjardim@gmail.com>
2024-05-13 21:35:37 -0300

    fix bug css height e ajustado rota lazy load

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/main/components/home/home.component';
-import { AboutComponent } from './pages/main/components/about/about.component';
 import { MainComponent } from './pages/main/main.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: MainComponent
+        loadComponent: () => import('./pages/main/main.component').then(c => c.MainComponent)
     }
 ];
