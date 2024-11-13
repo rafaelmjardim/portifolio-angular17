f3348b9b66d44bdac7c4caa8e4e28037abf4dea3
rafaelmjardim <rafaelmjardim@hotmail.com>
2024-11-13 10:49:24 -0300

    update link para projetos

--- src/app/pages/main/components/projects/projects.component.ts
+++ src/app/pages/main/components/projects/projects.component.ts
@@ -1,47 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Project } from './projects';
 import { MatDialog } from "@angular/material/dialog";
-import { ProjectDialogComponent } from './project-dialog/project-dialog.component';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { ionArrowForwardOutline } from '@ng-icons/ionicons';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [NgIcon],
+  providers: [
+    provideIcons({
+      ionArrowForwardOutline
+    })],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
 
-  projectsList: Project[] = [
+  protected projectsList: Project[] = [
     {
       url_img: 'assets/projects/cardapio-online.png',
       name: 'Cardapio Online',
       details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
-      link: '',
+      link: 'https://cardapio-online-project.vercel.app/',
       technologies: 'Angular, Typescript, SASS e Firebase'
     },
     {
       url_img: 'assets/projects/travel.png',
       name: 'Travel Landingpage',
       details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
-      link: '',
+      link: 'https://travel-website-v17-project.vercel.app/',
       technologies: 'Angular com SSR, HTML e SASS'
     },
     {
       url_img: 'assets/projects/todo.png',
       name: 'Todo List',
       details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
-      link: '',
+      link: 'https://todo-list-portfolio-project.vercel.app/todo',
       technologies: 'Angular, Typescript, HTML, SASS'
     },
     
   ];
 
-  constructor (private dialog: MatDialog){}
+  private router = inject(Router);
 
-  handleOpenDialog = (project: Project) => {
-    // this.dialog.open(ProjectDialogComponent);
-    return
+  openProjectLink = (url: string) => {
+    if (!url && !url.length) {
+      return
+    }
+    
+    window.open(url, '_blank')
   }
 
 }
